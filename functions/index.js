4789edc30b03336ff84f8b0697378ea32b39d8ab
0ranzjuice <q58dny@gmail.com>
2021-07-17 22:25:13 +0900

    몽고db연결

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
-const functions = require("firebase-functions");
 const express = require("express");
-const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51HPvTBIOljmntfcQ2sqWB66v8kmmy5YyMAL3Q2xNyKVZ48UeP3vVKMWhiUO0yoaC3XCeVq3NSTRiOCC4a43N33do004pCo3n1C"
-);
-
 const app = express();
+const cors = require("cors");
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://user1:sm0000@cluster0.jg6ur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('mongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.use(cors({ origin: true }));
 app.use(express.json());
 
@@ -13,20 +16,4 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/payments/create", async (req, res) => {
-  const total = req.query.total;
-
-  console.log(total);
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total,
-    currency: "usd",
-  });
-
-  res.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-//exports.api = functions.https.onRequest(app);
 app.listen(5000);
