630944165e3f82fceddcd136e3abfe7773b7d06a
skmwit <irenelyeo@gmail.com>
2021-09-05 18:33:02 +0900

    proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports= function(app){
     app.use(
-        createProxyMiddleware("/api", {
-            target:'http://13.124.139.165:3001',
+        createProxyMiddleware("/", {
+            target:'http://13.124.139.165:3001/',
             changeOrigin:true
         })
     );
